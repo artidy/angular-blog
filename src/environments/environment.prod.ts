31fe8ccd8585943328ce29c6a11ad217869f4a53
Andrey Pechennikov <ap@devandy.me>
2021-02-25 20:24:27 +0600

    create admin layout, add authorization and control

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
-export const environment = {
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  apiKey: "AIzaSyCD0oy90k7Xs1NY_RPSAkVExdTf144y4mI",
+  authDomain: "angular-blog-11a3e.firebaseapp.com",
+  projectId: "angular-blog-11a3e",
+  storageBucket: "angular-blog-11a3e.appspot.com",
+  messagingSenderId: "356447252271",
+  appId: "1:356447252271:web:29f894b1ac3b02462dbbda",
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,15 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  apiKey: "AIzaSyCD0oy90k7Xs1NY_RPSAkVExdTf144y4mI",
+  authDomain: "angular-blog-11a3e.firebaseapp.com",
+  projectId: "angular-blog-11a3e",
+  storageBucket: "angular-blog-11a3e.appspot.com",
+  messagingSenderId: "356447252271",
+  appId: "1:356447252271:web:29f894b1ac3b02462dbbda",
   production: false
 };
 
